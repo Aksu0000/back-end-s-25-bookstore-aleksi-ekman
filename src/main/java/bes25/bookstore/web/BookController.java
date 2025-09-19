462ccfab75dccbc113681dfa5431dedbfaa90e21
Aksu <a1303599@myy.haaga-helia.fi>
2025-09-19 10:23:48 +0300

    Bookstoren luonti

---
+++ src/main/java/bes25/bookstore/web/BookController.java
@@ -0,0 +1,17 @@
+package bes25.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class BookController {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "Welcome to the Bookstore!";
+    }
+
+}
