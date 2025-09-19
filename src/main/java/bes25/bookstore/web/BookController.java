b63b9638a63778b1a06f198b3f1748b812455e63
Aksu <a1303599@myy.haaga-helia.fi>
2025-09-19 18:21:36 +0300

    Lisätty edit toiminnallisuus

--- src/main/java/bes25/bookstore/web/BookController.java
+++ src/main/java/bes25/bookstore/web/BookController.java
@@ -3,9 +3,11 @@ package bes25.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 import bes25.bookstore.domain.Book;
@@ -35,15 +37,22 @@ public class BookController {
     }
 
     @PostMapping(value = "/save")
-    public String save(Book book) {
+    public String saveEdit( Book book) {
         repository.save(book);
-        return "redirect:booklist";
+        return "redirect:/booklist";
     }
 
     @GetMapping(value = "/delete/{id}")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         repository.deleteById(bookId);
         return "redirect:../booklist";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        Book book = repository.findById(bookId).get(); // haetaan kirja reposta
+        model.addAttribute("book", book); // lisätään malliobjekti "book", jota lomake käyttää
+        return "editbook"; // näkymä editbook.html
     }
 
     @GetMapping("/index")
