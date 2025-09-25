4b34b4470f0f7073634e3fd5fc5270d672306665
Aksu <a1303599@myy.haaga-helia.fi>
2025-09-26 02:27:18 +0300

    Lisätty Spring Data REST sekä PUT ja DELETE

--- src/main/java/bes25/bookstore/web/BookRestController.java
+++ src/main/java/bes25/bookstore/web/BookRestController.java
@@ -1,16 +1,17 @@
 package bes25.bookstore.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import bes25.bookstore.domain.Book;
 import bes25.bookstore.domain.BookRepository;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 //import org.springframework.web.bind.annotation.RequestBody;
 @RestController
@@ -29,7 +30,7 @@ public class BookRestController {
 
     //hae kirja id:n perusteella
     @GetMapping("/books/{id}")
-    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {
+    public Book getBookById(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId).orElse(null);
     }
 
@@ -38,5 +39,27 @@ public class BookRestController {
         return bookRepository.save(newBook);
     }
     
+    // päivitä olemassa oleva kirja JSON:illa
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> editBook(@RequestBody Book editedBook, @PathVariable Long id) {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    editedBook.setId(id);
+                    bookRepository.save(editedBook);
+                    return ResponseEntity.ok(editedBook);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Poista kirja ID:n perusteella
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable("id") Long bookId) {
+        if (bookRepository.existsById(bookId)) {
+            bookRepository.deleteById(bookId);
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
     
 }
